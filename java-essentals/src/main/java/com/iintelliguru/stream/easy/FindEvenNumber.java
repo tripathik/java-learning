db74274c3c86a3b01d17491956410d21d7beac75
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-17 00:15:20 +0530

    Find all the even number integer in an array using stream.

---
+++ java-essentals/src/main/java/com/iintelliguru/stream/easy/FindEvenNumber.java
@@ -0,0 +1,16 @@
+package com.iintelliguru.stream.easy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindEvenNumber {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(23, 52, 18, 27,98,10,9);
+        List<Integer> evenNumList = integerList.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
+        System.out.println("Even Numbers are:");
+        for(int num: evenNumList){
+            System.out.println(num);
+        }
+    }
+}
