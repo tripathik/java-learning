6fc7f4a4dd424dba220911851be3383c57d94589
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-17 00:34:37 +0530

    Find max num using stream max() function.

---
+++ java-essentals/src/main/java/com/iintelliguru/stream/easy/FindMaxNum.java
@@ -0,0 +1,12 @@
+package com.iintelliguru.stream.easy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FindMaxNum {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 9, 1, 7, 5);
+        int num = numbers.stream().max(Integer::compareTo).get();
+        System.out.println(" Maximum Number is: "+num);
+    }
+}
