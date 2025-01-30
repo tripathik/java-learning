002ba8ed19cfe3343c807713d73118c5f1bed3b6
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2025-01-30 23:45:29 +0530

    Understanding of volatile, atomic and synchronized keywords - with definition.

--- java-essentals/src/main/java/com/iintelliguru/thread/AtomicVolatileSynchronized.java
+++ java-essentals/src/main/java/com/iintelliguru/thread/AtomicVolatileSynchronized.java
@@ -9,7 +9,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class AtomicVolatileSynchronized {
     private volatile boolean flag;
+    // Volatile keyword is used, when we ensure that the particular variable may be modified by multiple threads and
+    // changes made to the variable is visible to other threads immediately.
+    // This also resolve the visibility problem.
+
     private AtomicInteger counter = new AtomicInteger(0);
+    //This keyword is useful when we need to update, increment or compare the variable without data race issue
+    // in thread safe environment
 
     public void setFlag(boolean value){
         flag = value;
@@ -27,6 +33,8 @@ public class AtomicVolatileSynchronized {
         return counter.get();
     }
 
+    // Using this keywords, only one thread can access the block of code at a time,
+    // remaining thread will be waiting until its lock over.
     public synchronized Data getDataValue(){ // Just an example to understand the synchronized, in real world, its not a feasible way.
         Data data = new Data();
         data.setDataFlag(flag);
